cdeb832b717f02537773a3a273cb222e7d50855d
Pablo <pablovivasgarcia1@gmail.com>
2021-10-08 13:58:19 +0200

    Residencia y SO

--- Prueba.java
+++ Prueba.java
@@ -5,6 +5,8 @@ public class Prueba {
 		System.out.println("Pablo Vivas García");
 		System.out.println("León");
 		System.out.println("1998");
+		System.out.println("Santa María del Páramo");
+		System.out.println("Windows");
 
 	 }
 
