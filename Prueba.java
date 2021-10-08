e77bdd86876a80fbf3bf958e928a5c0f0370cf20
Pablo <pablovivasgarcia1@gmail.com>
2021-10-08 14:24:42 +0200

    Sin nombre y con grupo favorito

--- Prueba.java
+++ Prueba.java
@@ -2,11 +2,11 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Pablo Vivas García");
 		System.out.println("León");
 		System.out.println("1998");
 		System.out.println("Santa María del Páramo");
 		System.out.println("Windows");
+		System.out.println("AC/DC");
 
 	 }
 
